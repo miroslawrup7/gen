6334451a8ac940b4b81dcec6496a9016580a6d54
Mirek Rup <miroslawrup7@gmail.com>
2024-04-02 18:10:18 +0200

    Initial & pages movement

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const containerLoc = document.querySelector(".container")
+const formPagesLoc = document.querySelector(".form-pages")
+const buttonNextLoc = document.querySelectorAll(".button.next")
+const buttonPrevLoc = document.querySelectorAll(".button.prev")
+
+buttonNextLoc.forEach((elem) => {
+    elem.addEventListener("click", ()=> {
+        const actualPosition = Number(getComputedStyle(formPagesLoc).left.slice(0, getComputedStyle(formPagesLoc).left.length-2))
+        const containerWidth = Number(getComputedStyle(containerLoc).width.slice(0, getComputedStyle(containerLoc).width.length-2))
+        const leftContainerBorder = Number(getComputedStyle(containerLoc).borderLeftWidth.slice(0, getComputedStyle(containerLoc).borderLeftWidth.length-2))
+        const rightContainerBorder = Number(getComputedStyle(containerLoc).borderRightWidth.slice(0, getComputedStyle(containerLoc).borderRightWidth.length-2))
+
+        const leftPagesBorder = Number(getComputedStyle(formPagesLoc).borderLeftWidth.slice(0, getComputedStyle(formPagesLoc).borderLeftWidth.length-2))
+        const rightPagesBorder = Number(getComputedStyle(formPagesLoc).borderRightWidth.slice(0, getComputedStyle(formPagesLoc).borderRightWidth.length-2))
+
+        const containerWidthWithoutBorder = containerWidth - leftContainerBorder - rightContainerBorder - leftPagesBorder - rightPagesBorder
+        const newLeftValue = actualPosition - containerWidthWithoutBorder
+
+        formPagesLoc.style.left = newLeftValue + "px"
+    })
+})
+
+buttonPrevLoc.forEach((elem) => {
+    elem.addEventListener("click", ()=> {
+        const actualPosition = Number(getComputedStyle(formPagesLoc).left.slice(0, getComputedStyle(formPagesLoc).left.length-2))
+        const containerWidth = Number(getComputedStyle(containerLoc).width.slice(0, getComputedStyle(containerLoc).width.length-2))
+        const leftContainerBorder = Number(getComputedStyle(containerLoc).borderLeftWidth.slice(0, getComputedStyle(containerLoc).borderLeftWidth.length-2))
+        const rightContainerBorder = Number(getComputedStyle(containerLoc).borderRightWidth.slice(0, getComputedStyle(containerLoc).borderRightWidth.length-2))
+
+        const leftPagesBorder = Number(getComputedStyle(formPagesLoc).borderLeftWidth.slice(0, getComputedStyle(formPagesLoc).borderLeftWidth.length-2))
+        const rightPagesBorder = Number(getComputedStyle(formPagesLoc).borderRightWidth.slice(0, getComputedStyle(formPagesLoc).borderRightWidth.length-2))
+
+        const containerWidthWithoutBorder = containerWidth - leftContainerBorder - rightContainerBorder - leftPagesBorder - rightPagesBorder
+        const newLeftValue = actualPosition + containerWidthWithoutBorder
+
+        formPagesLoc.style.left = newLeftValue + "px"
+    })
+})
